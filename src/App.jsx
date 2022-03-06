7936b57c3ec3dbcee28ef918ea03d158ab471962
efraincor <efracm15@gmail.com>
2022-03-06 12:57:47 -0500

    Primer commit -componente (definirPresupuesto)-

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+//Importando componenetes creados
+import Header from './components/Header'
+import NuevoPresupuesto from './components/NuevoPresupuesto'
+
+
+function App() {
+  
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  return (
+    <div>
+      <Header
+        presupuesto = {presupuesto}
+        setPresupuesto = {setPresupuesto}
+        isValidPresupuesto = {isValidPresupuesto}
+        setIsValidPresupuesto = {setIsValidPresupuesto}
+      />
+    </div>
+  )
+}
+
+export default App
